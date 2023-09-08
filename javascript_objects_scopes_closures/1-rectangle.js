bef5d46732892a6e3c26d909ac8dbc64f256c0b4
Martha Siiyabia Tong <marthasiiyatong@gmail.com>
2023-09-07 20:14:13 -0700

    1-rectangle.js

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+/* A class Rectangle that defines a rectangle with a width and height.
+*/
+
+class Rectangle {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+    }
+}
+
+module.exports = Rectangle;
