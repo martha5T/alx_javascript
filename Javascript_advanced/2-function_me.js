8ce0fe9212f2cc6c079e2392cf11d078e7d9440d
Martha Siiyabia Tong <marthasiiyatong@gmail.com>
2023-09-14 19:15:14 -0700

    update

--- Javascript_advanced/2-function_me.js
+++
@@ -1,8 +0,0 @@
-const alert = require('alert');
-function welcomeMessage (fullName) {
-    alert('Welcome ' + fullName);
-}
-
-const guillame = welcomeMessage('Guillame');
-const alex = welcomeMessage('Alex');
-const fred = welcomeMessage('Fred');
