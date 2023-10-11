b1a0355cc19fceac675ca05e6cbb46a38e7cae4b
Martha Siiyabia Tong <marthasiiyatong@gmail.com>
2023-10-10 20:31:19 -0700

    103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,9 +1,18 @@
-const myObject = {
+let myObject = {
     type: 'object',
     value: 12,
-    incr: function() {
-        this.value++;
-    }
 };
+  console.log(myObject);
+  /*
+  YOUR CODE HERE
+  */
+myObject.incr = function () {
+    this.value++;
+}
 
-console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
